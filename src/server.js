fd7f87925d2a032d973fdec2deab484acd3c0a77
Wakil Bouljoub <abouljoub@gmail.com>
2022-01-14 20:53:40 +0200

    Add MongoDb dependencies with a quick server tests

--- src/server.js
+++ src/server.js
@@ -1,5 +1,27 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { MongoClient } from "mongodb";
 
 const app = express();
 
-app.listen(8000, () => console.log("gooodd"));
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+app.use(bodyParser.json());
+
+app.get("/api/user/:name", async (req, res) => {
+  const username = req.params.name;
+
+  const client = await MongoClient.connect("mongodb://localhost:27017");
+  const db = client.db("solifarmer");
+
+  const userInfo = await db.collection("farms").findOne({ name: username });
+  res.status(200).json(userInfo);
+
+  client.close();
+});
+app.get("/hello/:name", (req, res) => res.send(`Hello! ${req.params.name}`));
+app.get("/hello", (req, res) => res.send(`Hello! ${req.body.name}`));
